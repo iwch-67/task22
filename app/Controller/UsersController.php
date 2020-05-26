275e2871d41faf339bbc25b9897be31077612ddf
iwch-67 <gekiuma4@gmail.com>
2020-05-26 17:24:53 +0900

    UsersControllerのアクション設定

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,46 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class UsersController extends AppController {
+	public function beforeFilter() {
+		parent::beforeFilter();
+		//ユーザー自身による登録とログアウトを許可する
+		$this->Auth->allow('add', 'login', 'logout');
+	}
+	public function login() {
+		//フォームから情報が送信された場合、認証を実施
+		if ($this->request->is('post')) {
+			//認証処理を実施
+			if ($this->Auth->login()) {
+				$this->Flash->success(__('ログインに成功しました'));
+				return $this->redirect($this->Auth->redirect());
+			} else {
+				$this->Flash->error(__('メールアドレスまたはパスワードが違います。再度お試しください'));
+			}
+		}
+	}
+	public function logout() {
+		$this->redirect($this->Auth->logout());
+	}
+
+	public function index() {
+		$this->set('posts', $this->Blog->find('all'));
+		$this->set('title_for_layout', '記事一覧');
+	}
+
+	public function add() {
+		$this->set('title_for_layout', '新規登録');
+		if ($this->request->is('post')) {
+			$this->User->create();
+			if ($this->User->save($this->request->data)) {
+				$this->Flash->success(__('ユーザー登録されました'));
+				return $this->redirect(array(
+					'controller' => 'blogs',
+					'action' => 'index'
+				));
+			}
+			$this->Flash->error(__('ユーザー登録に失敗しました。再度お試しください'));
+		}
+	}
+}
+?>
