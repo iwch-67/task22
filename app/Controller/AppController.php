d0b19b15683b209f3e2b47d6873fbb1cc38193a3
iwch-67 <gekiuma4@gmail.com>
2020-05-26 17:20:05 +0900

    beforefilterなどの各種設定

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,5 +31,38 @@ App::uses('Controller', 'Controller');
  * @link		https://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	public $components = array('DebugKit.Toolbar');
+	public $components = array(
+		'DebugKit.Toolbar',
+		'Flash',
+		'Auth' => array(
+			'loginRedirect' => array(
+				'controller' => 'blogs',
+				'action' => 'index'
+			),
+			'logoutRedirect' => array(
+				'controller' => 'pages',
+				'action' => 'display',
+				'home'
+			),
+			'authenticate' => array(
+				'Form' => array(
+					'passwordHasher' => 'Blowfish',
+					'fields' => array(
+						'username' => 'email'
+					)
+				)
+			)
+		)
+	);
+	public function isAuthorized($user) {
+		if (isset($user['role']) && $user['role'] === 'admin') {
+			return true;
+		}
+		return false;
+	}
+
+	public function beforeFilter() {
+		$this->Auth->allow('index', 'view');
+	}
 }
+
