9431b92a69cd80e4a02a578c07afb6596471fed3
iwch-67 <gekiuma4@gmail.com>
2020-05-27 20:14:37 +0900

    リレーションや認証メソッドの追記

--- app/Model/Blog.php
+++ app/Model/Blog.php
@@ -1,5 +1,15 @@
 <?php
 class Blog extends AppModel {
+	public $belongsTo = array('User');
+
+	public function isOwnedBy($post, $user) {
+		return $this->field('id',
+			array(
+				'id' => $post,
+				'user_id' => $user
+			)
+		) !== false;
+	}
 	public $validate = array(
 		'title' => array(
 			'rule' => 'notBlank'
